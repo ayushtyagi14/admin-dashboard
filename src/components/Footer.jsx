182fab260381612fc8daa8f82ebd551939b41603
Ayush Tyagi <ayush.tyagi145@gmail.com>
2022-07-24 02:25:17 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 All rights reserved by Dashboard
+      <br />
+      Created by{" "}
+      <a
+        href="https://portfolio-ayushtyagi14.vercel.app/"
+        target="_blank"
+        className="font-semibold"
+      >
+        Ayush Tyagi
+      </a>
+    </p>
+  </div>
+);
+
+export default Footer;
